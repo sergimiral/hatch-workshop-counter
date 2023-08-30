c37327b9a53cce5db32682f6532418838df7e28b
Sergi Miral <sergimiral@gmail.com>
2023-08-30 13:56:50 +0200

    aider: Removed current and max participants and added a label based on the status of each progress bar.
    # Aider chat conversation:
    
    USER: I'd like to remove the <p> tags in the workshop module and add a label based on the status of each progress bar:
    If two or less show "moderate".
    If three show "full".
    If four show "very full".
    If five show "packed".
    
    ASSISTANT: To implement this, we need to modify the `Workshop.js` file. We will remove the `<p>` tags that display the current and max participants and add a new `<p>` tag that displays a label based on the status of each progress bar.
    
    Here's how we can adjust the Workshop component:
    
    1. Remove the `<p>` tags that display the current and max participants.
    2. Add a new `<p>` tag that displays a label based on the status of each progress bar. We can use a function to determine the label.
    
    Here's the *edit block* for the `Workshop.js` file:
    
    ```javascript
    components/Workshop.js
    <<<<<<< HEAD
      return (
        <div className={styles.workshop}>
          <h2>{Name.title[0].text.content}</h2>
          <ProgressBar current={Current.number} max={Max.number} />
          <p>Current Participants: {Current.number}</p>
          <p>Max Participants: {Max.number}</p>
        </div>
      );
    =======
      const getProgressBarLabel = () => {
        if (Current.number <= 2) return 'Moderate';
        if (Current.number === 3) return 'Full';
        if (Current.number === 4) return 'Very Full';
        return 'Packed';
      };
    
      return (
        <div className={styles.workshop}>
          <h2>{Name.title[0].text.content}</h2>
          <ProgressBar current={Current.number} max={Max.number} />
          <p>{getProgressBarLabel()}</p>
        </div>
      );
    >>>>>>> updated
    ```
    
    This change will remove the current and max participants from the Workshop component and add a label based on the status of each progress bar.

--- components/Workshop.js
+++ components/Workshop.js
@@ -4,12 +4,18 @@ import ProgressBar from './ProgressBar';
 
 const Workshop = ({ workshop }) => {
   const { Name, Current, Max } = workshop.properties;
+  const getProgressBarLabel = () => {
+    if (Current.number <= 2) return 'Moderate';
+    if (Current.number === 3) return 'Full';
+    if (Current.number === 4) return 'Very Full';
+    return 'Packed';
+  };
+
   return (
     <div className={styles.workshop}>
       <h2>{Name.title[0].text.content}</h2>
       <ProgressBar current={Current.number} max={Max.number} />
-      <p>Current Participants: {Current.number}</p>
-      <p>Max Participants: {Max.number}</p>
+      <p>{getProgressBarLabel()}</p>
     </div>
   );
 };
