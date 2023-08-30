a7870604d196244ce82e2a2c28ebb3041e746dae
Sergi Miral <sergimiral@gmail.com>
2023-08-30 11:44:40 +0200

    Refactor index.js to include a Notion client and database ID, and update the header title.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+require('dotenv').config();
+const { Client } = require('@notionhq/client');
+
+const notion = new Client({ auth: process.env.NOTION_KEY });
+const databaseId = process.env.NOTION_DATABASE_ID;
 
 export default function Home() {
+  console.log(notion.databases.list);
   return (
     <div className="container">
       <Head>
@@ -11,7 +17,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
@@ -19,5 +25,5 @@ export default function Home() {
 
       <Footer />
     </div>
-  )
+  );
 }
