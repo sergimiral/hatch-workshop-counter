f2e869da12636c2db4f854b37b4e0625c45ee9e2
Sergi Miral <sergimiral@gmail.com>
2023-08-30 13:47:19 +0200

    aider: Revert changes made to the ProgressBar component and associated CSS module.
    # Aider chat conversation:
    
    USER: Undo the last changes
    
    ASSISTANT: To undo the last changes, we need to revert the changes made in the `ProgressBar` component and the associated CSS module.
    
    In the `ProgressBar` component, we will change the `getProgressBarColor` function back to `getSegmentColor` which will return a color based on the index of the segment. We will then apply this color to the individual segments instead of the `progressBar` div.
    
    In the CSS module, we will add back the individual color classes and remove the `filled` class.
    
    Here are the changes:
    
    **components/ProgressBar.js**
    ```python
    components/ProgressBar.js
    <<<<<<< HEAD
    const ProgressBar = ({ current, max }) => {
      const segments = Array.from({ length: 5 }, (_, i) => i < current);
    
      const getProgressBarColor = () => {
        if (current === 0) return styles.green;
        if (current < 3) return styles.yellow;
        return styles.red;
      };
    
      return (
        <div className={`${styles.progressBar} ${getProgressBarColor()}`}>
          {segments.map((isActive, i) => (
            <div
              key={i}
              className={`${styles.segment} ${isActive ? styles.filled : ''}`}
            />
          ))}
        </div>
      );
    };
    =======
    const ProgressBar = ({ current, max }) => {
      const segments = Array.from({ length: 5 }, (_, i) => i < current);
    
      const getSegmentColor = (index) => {
        if (index === 0) return styles.green;
        if (index < 3) return styles.yellow;
        return styles.red;
      };
    
      return (
        <div className={styles.progressBar}>
          {segments.map((isActive, i) => (
            <div
              key={i}
              className={`${styles.segment} ${isActive ? getSegmentColor(i) : ''}`}
            />
          ))}
        </div>
      );
    };
    >>>>>>> updated
    ```
    
    **components/ProgressBar.module.css**
    ```python
    components/ProgressBar.module.css
    <<<<<<< HEAD
    .filled {
      background-color: #f3f3f3;
    }
    =======
    .green {
      background-color: #4caf50;
    }
    
    .yellow {
      background-color: #ffeb3b;
    }
    
    .red {
      background-color: #f44336;
    }
    >>>>>>> updated
    ```

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -4,18 +4,18 @@ import styles from './ProgressBar.module.css';
 const ProgressBar = ({ current, max }) => {
   const segments = Array.from({ length: 5 }, (_, i) => i < current);
 
-  const getProgressBarColor = () => {
-    if (current === 0) return styles.green;
-    if (current < 3) return styles.yellow;
+  const getSegmentColor = (index) => {
+    if (index === 0) return styles.green;
+    if (index < 3) return styles.yellow;
     return styles.red;
   };
 
   return (
-    <div className={`${styles.progressBar} ${getProgressBarColor()}`}>
+    <div className={styles.progressBar}>
       {segments.map((isActive, i) => (
         <div
           key={i}
-          className={`${styles.segment} ${isActive ? styles.filled : ''}`}
+          className={`${styles.segment} ${isActive ? getSegmentColor(i) : ''}`}
         />
       ))}
     </div>
