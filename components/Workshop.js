0e91aee9aa91b3a95f9333295de19f3e3a4bbc29
Sergi Miral <sergimiral@gmail.com>
2023-08-30 12:09:38 +0200

    aider: Make the main section fill the whole viewport, make each workshop item take as much available screen size as they can, and add a little space between them.
    # Aider chat conversation:
    
    USER: Make the main section to fill the whole viewport. Make each workshop item to take as much available screen size as they can. Workshop items should have a black background and white text. The items inside the main section should have a little space between them using margin or padding.
    
    ASSISTANT: To achieve this, we need to modify the CSS styles for the `main` section and the `Workshop` component.
    
    First, let's add a new CSS module for the `Workshop` component. We'll create a new file `components/Workshop.module.css`:
    
    ```css
    components/Workshop.module.css
    <<<<<<< HEAD
    =======
    .workshop {
      background-color: black;
      color: white;
      margin: 10px;
      padding: 10px;
      flex: 1;
    }
    >>>>>>> updated
    ```
    
    Then, we need to import this CSS module in the `Workshop` component and apply the `workshop` class to the root `div`:
    
    ```javascript
    components/Workshop.js
    <<<<<<< HEAD
    import React from 'react';
    
    const Workshop = ({ workshop }) => {
      const { Name, Current, Max } = workshop.properties;
      return (
        <div>
          <h2>{Name.title[0].text.content}</h2>
          <p>Current Participants: {Current.number}</p>
          <p>Max Participants: {Max.number}</p>
        </div>
      );
    };
    
    export default Workshop;
    =======
    import React from 'react';
    import styles from './Workshop.module.css';
    
    const Workshop = ({ workshop }) => {
      const { Name, Current, Max } = workshop.properties;
      return (
        <div className={styles.workshop}>
          <h2>{Name.title[0].text.content}</h2>
          <p>Current Participants: {Current.number}</p>
          <p>Max Participants: {Max.number}</p>
        </div>
      );
    };
    
    export default Workshop;
    >>>>>>> updated
    ```
    
    Finally, we need to modify the `main` section styles in the `styles/globals.css` file:
    
    ```css
    styles/globals.css
    <<<<<<< HEAD
    main {
      padding: 5rem 0;
      flex: 1;
      display: flex;
      flex-direction: column;
      justify-content: center;
      align-items: center;
    }
    =======
    main {
      padding: 5rem 0;
      flex: 1;
      display: flex;
      flex-direction: column;
      justify-content: center;
      align-items: center;
      height: 100vh;
    }
    >>>>>>> updated
    ```
    
    This will make the `main` section fill the whole viewport and each `Workshop` item take as much available screen size as they can. The `Workshop` items will have a black background and white text, and there will be a little space between them.

--- components/Workshop.js
+++ components/Workshop.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './Workshop.module.css';
 
 const Workshop = ({ workshop }) => {
   const { Name, Current, Max } = workshop.properties;
   return (
-    <div>
+    <div className={styles.workshop}>
       <h2>{Name.title[0].text.content}</h2>
       <p>Current Participants: {Current.number}</p>
       <p>Max Participants: {Max.number}</p>
