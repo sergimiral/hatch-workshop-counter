bf71a4cfcd29326ee54910ed800d7381b0c90032
Sergi Miral <sergimiral@gmail.com>
2023-08-30 13:17:02 +0200

    aider: Refactored `pages/index.js` to use the `useEffect` hook to fetch workshops data every 5 seconds and update the state.
    # Aider chat conversation:
    
    USER: I want to refresh the content or the page every 5 seconds. I am deploying to Netlify so I can't just paste a setTimeout for refresh in the main thread I think… I'm looking for simple solutions to refresh the content or the page every once and then,.
    
    ASSISTANT: To refresh the content of a page every 5 seconds, you can use the `useEffect` hook in React. This hook allows you to perform side effects in function components. In this case, the side effect would be refreshing the content of the page.
    
    However, I need to modify the main component file where the content is rendered. Could you please add the main component file to the chat so I can propose the necessary changes?
    
    USER: `pages/index.js` `components/Workshop.js`
    
    ASSISTANT: Great, we can use the `useEffect` hook in the `pages/index.js` file to fetch the data every 5 seconds.
    
    Here are the changes:
    
    ```javascript
    pages/index.js
    <<<<<<< HEAD
    export default function Home({ workshops }) {
      return (
        <div className="container">
          <Head>
            <title>Next.js Starter!</title>
            <link rel="icon" href="/favicon.ico" />
          </Head>
    
          <main>
            {workshops.map((workshop, index) => (
              <Workshop key={index} workshop={workshop} />
            ))}
          </main>
    
          {/* <Footer /> */}
        </div>
      );
    }
    
    export async function getServerSideProps() {
      const notion = new Client({
        auth: process.env.NEXT_PUBLIC_NOTION_KEY,
      });
      const databaseId = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID;
      const workshops = await notion.databases.query({
        database_id: databaseId,
      });
      return {
        props: {
          workshops: workshops.results,
        },
      };
    }
    =======
    import { useEffect, useState } from 'react';
    
    export default function Home() {
      const [workshops, setWorkshops] = useState([]);
    
      useEffect(() => {
        const fetchWorkshops = async () => {
          const notion = new Client({
            auth: process.env.NEXT_PUBLIC_NOTION_KEY,
          });
          const databaseId = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID;
          const response = await notion.databases.query({
            database_id: databaseId,
          });
          setWorkshops(response.results);
        };
    
        fetchWorkshops();
        const intervalId = setInterval(fetchWorkshops, 5000); // fetch every 5 seconds
    
        return () => clearInterval(intervalId); // cleanup on unmount
      }, []);
    
      return (
        <div className="container">
          <Head>
            <title>Next.js Starter!</title>
            <link rel="icon" href="/favicon.ico" />
          </Head>
    
          <main>
            {workshops.map((workshop, index) => (
              <Workshop key={index} workshop={workshop} />
            ))}
          </main>
    
          {/* <Footer /> */}
        </div>
      );
    }
    >>>>>>> updated
    ```
    
    This change will fetch the workshops data every 5 seconds and update the state, causing a re-render of the `Home` component with the new data. The `useEffect` hook will also clean up the interval when the component is unmounted, preventing memory leaks.

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,29 @@ import Workshop from '@components/Workshop';
 require('dotenv').config();
 const { Client } = require('@notionhq/client');
 
-export default function Home({ workshops }) {
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+  const [workshops, setWorkshops] = useState([]);
+
+  useEffect(() => {
+    const fetchWorkshops = async () => {
+      const notion = new Client({
+        auth: process.env.NEXT_PUBLIC_NOTION_KEY,
+      });
+      const databaseId = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID;
+      const response = await notion.databases.query({
+        database_id: databaseId,
+      });
+      setWorkshops(response.results);
+    };
+
+    fetchWorkshops();
+    const intervalId = setInterval(fetchWorkshops, 5000); // fetch every 5 seconds
+
+    return () => clearInterval(intervalId); // cleanup on unmount
+  }, []);
+
   return (
     <div className="container">
       <Head>
@@ -22,19 +44,4 @@ export default function Home({ workshops }) {
       {/* <Footer /> */}
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  const notion = new Client({
-    auth: process.env.NEXT_PUBLIC_NOTION_KEY,
-  });
-  const databaseId = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID;
-  const workshops = await notion.databases.query({
-    database_id: databaseId,
-  });
-  return {
-    props: {
-      workshops: workshops.results,
-    },
-  };
 }
