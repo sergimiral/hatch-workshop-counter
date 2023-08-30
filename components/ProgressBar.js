331d6a02f83b58008765a8b4b7a472fd618ffe75
Sergi Miral <sergimiral@gmail.com>
2023-08-30 13:50:32 +0200

    aider: Changed the progress bar to apply the colors to the squares inside each progress bar instead of the whole progress bar.
    # Aider chat conversation:
    
    USER: Right now the green, yellow and red colors are being applied to the progressbar, but I wanted to be applied to the squares inside each progressbar instead.
    
    ASSISTANT: To apply the colors to the squares inside each progress bar instead of the whole progress bar, you need to modify the `ProgressBar.js` and `ProgressBar.module.css` files.
    
    In `ProgressBar.js`, you need to change the `getProgressBarColor` function back to `getSegmentColor` and apply the color to the individual segments instead of the `progressBar` div. The updated code should look like this:
    
    ```javascript
    components/ProgressBar.js
    <<<<<<< HEAD
    const ProgressBar = ({ current, max }) => {
      const segments = Array.from({ length: 5 }, (_, i) => i < current);
    
      const getProgressBarColor = () => {
        if (current === max) return styles.green;
        if (current >= max / 2) return styles.yellow;
        return styles.red;
      };
    
      return (
        <div className={`${styles.progressBar} ${getProgressBarColor()}`}>
          {segments.map((isActive, i) => (
            <div
              key={i}
              className={`${styles.segment} ${isActive ? styles.filled : ''}`}
            />
          ))}
        </div>
      );
    };
    =======
    const ProgressBar = ({ current, max }) => {
      const segments = Array.from({ length: 5 }, (_, i) => i < current);
    
      const getSegmentColor = (isActive) => {
        if (!isActive) return '';
        if (current === max) return styles.green;
        if (current >= max / 2) return styles.yellow;
        return styles.red;
      };
    
      return (
        <div className={styles.progressBar}>
          {segments.map((isActive, i) => (
            <div
              key={i}
              className={`${styles.segment} ${getSegmentColor(isActive)}`}
            />
          ))}
        </div>
      );
    };
    >>>>>>> updated
    ```
    
    In `ProgressBar.module.css`, you need to add the individual color classes back and remove the `filled` class. The updated code should look like this:
    
    ```css
    components/ProgressBar.module.css
    <<<<<<< HEAD
    .segment {
      flex: 1;
      height: 20px;
      background-color: #f3f3f3;
      margin: 0 2px;
    }
    
    .filled {
      background-color: white;
    }
    
    .progressBar.green {
      background-color: #4caf50;
    }
    
    .progressBar.yellow {
      background-color: #ffeb3b;
    }
    
    .progressBar.red {
      background-color: #f44336;
    }
    =======
    .segment {
      flex: 1;
      height: 20px;
      background-color: #f3f3f3;
      margin: 0 2px;
    }
    
    .green {
      background-color: #4caf50;
    }
    
    .yellow {
      background-color: #ffeb3b;
    }
    
    .red {
      background-color: #f44336;
    }
    >>>>>>> updated
    ```
    
    These changes will apply the color to the individual segments based on the `current` prop.

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -4,18 +4,19 @@ import styles from './ProgressBar.module.css';
 const ProgressBar = ({ current, max }) => {
   const segments = Array.from({ length: 5 }, (_, i) => i < current);
 
-  const getProgressBarColor = () => {
+  const getSegmentColor = (isActive) => {
+    if (!isActive) return '';
     if (current === max) return styles.green;
     if (current >= max / 2) return styles.yellow;
     return styles.red;
   };
 
   return (
-    <div className={`${styles.progressBar} ${getProgressBarColor()}`}>
+    <div className={styles.progressBar}>
       {segments.map((isActive, i) => (
         <div
           key={i}
-          className={`${styles.segment} ${isActive ? styles.filled : ''}`}
+          className={`${styles.segment} ${getSegmentColor(isActive)}`}
         />
       ))}
     </div>
