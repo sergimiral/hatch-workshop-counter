335243c150273e850e9a6fac947182e7bdc0ed71
Sergi Miral <sergimiral@gmail.com>
2023-08-30 13:45:52 +0200

    aider: Changed the color of the progress bar to be applied to the whole bar instead of individual segments.
    # Aider chat conversation:
    
    USER: I want the color to be applied to the whole progress bar, not to the individual squares.
    
    ASSISTANT: To apply the color to the whole progress bar instead of individual squares, we need to modify the `ProgressBar` component and the associated CSS module.
    
    In the `ProgressBar` component, we will change the `getSegmentColor` function to `getProgressBarColor` which will return a color based on the `current` prop. We will then apply this color to the `progressBar` div instead of individual segments.
    
    In the CSS module, we will remove the individual color classes and add a `filled` class that will be used to apply the color to the progress bar.
    
    Here are the changes:
    
    **components/ProgressBar.js**
    ```python
    components/ProgressBar.js
    <<<<<<< HEAD
    const ProgressBar = ({ current, max }) => {
      const segments = Array.from({ length: 5 }, (_, i) => i < current);
    
      const getSegmentColor = (index) => {
        if (index === 0) return styles.green;
        if (index < 3) return styles.yellow;
        return styles.red;
      };
    
      return (
        <div className={styles.progressBar}>
          {segments.map((isActive, i) => (
            <div
              key={i}
              className={`${styles.segment} ${isActive ? getSegmentColor(i) : ''}`}
            />
          ))}
        </div>
      );
    };
    =======
    const ProgressBar = ({ current, max }) => {
      const segments = Array.from({ length: 5 }, (_, i) => i < current);
    
      const getProgressBarColor = () => {
        if (current === 0) return styles.green;
        if (current < 3) return styles.yellow;
        return styles.red;
      };
    
      return (
        <div className={`${styles.progressBar} ${getProgressBarColor()}`}>
          {segments.map((isActive, i) => (
            <div
              key={i}
              className={`${styles.segment} ${isActive ? styles.filled : ''}`}
            />
          ))}
        </div>
      );
    };
    >>>>>>> updated
    ```
    
    **components/ProgressBar.module.css**
    ```python
    components/ProgressBar.module.css
    <<<<<<< HEAD
    .green {
      background-color: #4caf50;
    }
    
    .yellow {
      background-color: #ffeb3b;
    }
    
    .red {
      background-color: #f44336;
    }
    =======
    .filled {
      background-color: #f3f3f3;
    }
    >>>>>>> updated
    ```

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -4,18 +4,18 @@ import styles from './ProgressBar.module.css';
 const ProgressBar = ({ current, max }) => {
   const segments = Array.from({ length: 5 }, (_, i) => i < current);
 
-  const getSegmentColor = (index) => {
-    if (index === 0) return styles.green;
-    if (index < 3) return styles.yellow;
+  const getProgressBarColor = () => {
+    if (current === 0) return styles.green;
+    if (current < 3) return styles.yellow;
     return styles.red;
   };
 
   return (
-    <div className={styles.progressBar}>
+    <div className={`${styles.progressBar} ${getProgressBarColor()}`}>
       {segments.map((isActive, i) => (
         <div
           key={i}
-          className={`${styles.segment} ${isActive ? getSegmentColor(i) : ''}`}
+          className={`${styles.segment} ${isActive ? styles.filled : ''}`}
         />
       ))}
     </div>
